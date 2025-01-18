f6007b258a50a856d538df732e122f89732fc81b
juan ortega <jortegah79@gmail.com>
2025-01-18 10:26:24 +0100

    subiendo a git

--- src/helpers/getGIfs.js
+++ src/helpers/getGIfs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=FTLuXezOpqJN4hqmeCEcog9EtIVdPTTD&q=${category}&limit=12`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=FTLuXezOpqJN4hqmeCEcog9EtIVdPTTD&q=${category}&limit=12`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(gif => ({
